a1c2323b8eb42848549692a1902ee187533e61b0
Le7o <7demijolla@gmail.com>
2020-07-03 09:57:12 +0200

    first commit

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyController {
+
+    //Ajouts pour tester le primary
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        System.out.println("Hello World");
+        // return "Hi folks";
+        //Changement de return pour tester primary
+        return greetingService.sayGreeting();
+
+
+    }
+}
