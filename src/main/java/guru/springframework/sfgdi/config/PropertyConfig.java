059090db2b444f7cffbbe6dcf4bb59c143e0d0ac
Le7o <7demijolla@gmail.com>
2020-07-06 10:41:28 +0200

    Test Env Property

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.exemplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    //Test Env Properties
+    @Autowired
+    Environment env;
 
     @Value("${afcepf.username}")
     String user;
@@ -23,7 +29,10 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        //Test PropertySource
+       // fakeDataSource.setUser(user);
+        //Test Env Properties
+        fakeDataSource.setUser(env.getProperty("JAVA_HOME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
